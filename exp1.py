5e90f9e2ae84e25db52c5ae0862aea21451592d3
OLEKSII ARIASOV <oleksii.ariasov@student.uni-tuebingen.de>
2025-06-17 13:48:53 +0200

    update345

--- exp1.py
+++ exp1.py
@@ -174,7 +174,8 @@ if __name__ == '__main__':
         prefixes = [trial_instruction] * 6
         queries = [obj_1, obj_2, obj_3, pictures[0], pictures[1], pictures[2]]
 
-        logs_probs = scorer_llama.conditional_score(prefixes, queries)
+        encoded = scorer_llama.tokenizer(prefixes, queries, padding=True, return_tensors="pt").to("cuda")
+        logs_probs = scorer_llama.compute_stats(encoded=encoded)
         print(trial_instruction)
         new_logs = [logs_probs[0] + logs_probs[3], logs_probs[1] + logs_probs[4], logs_probs[2] + logs_probs[5]]
         print(logs_probs)
